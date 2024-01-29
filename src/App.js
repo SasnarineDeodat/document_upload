3fd1c6a2bb018a89d30747bf51df123b551ae994
Robbie213 <sasnarinedeodat407@gmail.com>
2024-01-29 17:29:59 -0500

    rendered the component in the wrong spot

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import DocumentUpload from "./components/DocumentUpload";
 import Form from "./components/Form";
-import RightPart from "./components/RightPart";
 
 const App = () => {
   return (
     <div className="app">
       <DocumentUpload />
       <Form />
-      <RightPart />
     </div>
   );
 };
