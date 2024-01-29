df5668d9c00b6e60ba3df331a45b4463bcd031d4
Robbie213 <sasnarinedeodat407@gmail.com>
2024-01-29 15:37:12 -0500

    added css no logic

--- src/components/RightPart.js
+++ src/components/RightPart.js
@@ -6,7 +6,7 @@ const RightPart = () => {
       <div className="RightPart-child">
         <div className="RightPart-p1">
           <span>Split schedule using social distanting?</span>
-          <div>
+          <div className="swichers">
             <div>
               <input type="radio" value={"Yes"} />
               <span>Yes</span>
@@ -18,12 +18,12 @@ const RightPart = () => {
           </div>
         </div>
         <div className="RightPart-p2">
-          <span>Location checking:</span>
+          <h3>Location checking:</h3>
           <span>All Available!</span>
         </div>
-        <div className="RightPart-p3">
+        <div className="RightPart-p3" style={{ padding: "15px 0 15px 0" }}>
           <span>Client:</span>
-          <div>
+          <div className="swichers2">
             <div>
               <input type="radio" value={"Single"} />
               <span>Single</span>
@@ -33,45 +33,152 @@ const RightPart = () => {
               <span>Multiple</span>
             </div>
           </div>
-          <div>
-            <div>
+          <div
+            className="testers"
+            style={{ gap: "15px", display: "flex", flexDirection: "column" }}
+          >
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+              className="testers-container"
+            >
               <span>Testing Center 1</span>
-              <div>
-                <div>
-                  <span>Select Client</span>
-                  <img src="./down.png" />
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  position: "relative",
+                  right: "-20px",
+                  gap: 10,
+                }}
+              >
+                <div
+                  style={{
+                    padding: "10px",
+                    border: "rgb(19, 68, 97) thin solid",
+                    borderRadius: 10,
+                    display: "flex",
+                    gap: 10,
+                    alignItems: "center",
+                  }}
+                >
+                  <span style={{ color: "rgb(19, 68, 97)" }}>
+                    Select Client
+                  </span>
+                  <img height={13} width={13} src="./down.png" />
                 </div>
-                <img src="./clock.png" />
+                <img height={25} width={25} src="./clock.png" />
               </div>
             </div>
-            <div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+              className="testers-container"
+            >
               <span>Testing Center 2</span>
-              <div>
-                <div>
-                  <span>Select Client</span>
-                  <img src="./down.png" />
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  position: "relative",
+                  right: "-20px",
+                  gap: 10,
+                }}
+              >
+                <div
+                  style={{
+                    padding: "10px",
+                    border: "rgb(19, 68, 97) thin solid",
+                    borderRadius: 10,
+                    display: "flex",
+                    gap: 10,
+                    alignItems: "center",
+                  }}
+                >
+                  <span style={{ color: "rgb(19, 68, 97)" }}>
+                    Select Client
+                  </span>
+                  <img height={13} width={13} src="./down.png" />
                 </div>
-                <img src="./clock.png" />
+                <img height={25} width={25} src="./clock.png" />
               </div>
             </div>
-            <div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+              className="testers-container"
+            >
               <span>Testing Center 3</span>
-              <div>
-                <div>
-                  <span>Select Client</span>
-                  <img src="./down.png" />
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  position: "relative",
+                  right: "-20px",
+                  gap: 10,
+                }}
+              >
+                <div
+                  style={{
+                    padding: "10px",
+                    border: "rgb(19, 68, 97) thin solid",
+                    borderRadius: 10,
+                    display: "flex",
+                    gap: 10,
+                    alignItems: "center",
+                  }}
+                >
+                  <span style={{ color: "rgb(19, 68, 97)" }}>
+                    Select Client
+                  </span>
+                  <img height={13} width={13} src="./down.png" />
                 </div>
-                <img src="./clock.png" />
+                <img height={25} width={25} src="./clock.png" />
               </div>
             </div>
-            <div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+              className="testers-container"
+            >
               <span>Testing Center 4</span>
-              <div>
-                <div>
-                  <span>Select Client</span>
-                  <img src="./down.png" />
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  position: "relative",
+                  right: "-20px",
+                  gap: 10,
+                }}
+              >
+                <div
+                  style={{
+                    padding: "10px",
+                    border: "rgb(19, 68, 97) thin solid",
+                    borderRadius: 10,
+                    display: "flex",
+                    gap: 10,
+                    alignItems: "center",
+                  }}
+                >
+                  <span style={{ color: "rgb(19, 68, 97)" }}>
+                    Select Client
+                  </span>
+                  <img height={13} width={13} src="./down.png" />
                 </div>
-                <img src="./clock.png" />
+                <img height={25} width={25} src="./clock.png" />
               </div>
             </div>
           </div>
