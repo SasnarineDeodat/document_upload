704948838fcea8a6d07e3b1329e48e7781b86c13
Robbie213 <sasnarinedeodat407@gmail.com>
2024-01-29 17:00:39 -0500

    worked on the css a little more still have to make it responsive

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,28 @@
 import "./Form.css";
 import Select from "./Select";
 import UploadField from "./UploadField";
+import RightPart from "./RightPart";
 
 const Form = () => {
   return (
-    <div className="main_form_div">
-      <Select fields={["React", "Node"]} />
-      <hr style={{ width: "300px" }} />
-      <p className="upload_heading">
-        Select a manifest that you'd like to import
-      </p>
-      <UploadField />
+    <div style={{ display: "flex" }}>
+      <div className="main_form_div">
+        <Select fields={["React", "Node"]} />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "100%",
+            gap: 20,
+          }}
+        >
+          <p className="upload_heading">
+            Select a manifest that you'd like to import
+          </p>
+          <UploadField />
+        </div>
+      </div>
+      <RightPart />
     </div>
   );
 };
--- src/components/UploadField.js
+++ src/components/UploadField.js
@@ -1,20 +1,24 @@
 import document_icon from "../assets/document_icon.svg";
 import "./UploadField.css";
+
 const UploadField = () => {
   const handleImageUpload = () => {
     document.getElementById("fileUpload").click();
   };
+
   return (
-    <div>
-      <label for="images" class="drop-container" id="dropcontainer">
-        <img
-          src={document_icon}
-          alt="document_icon"
-          className="document_icon"
-        />
-        <span className="drop-title">
-          Drop & Drop Here or <strong>Browse</strong>
-        </span>
+    <div className="outer-div">
+      <label htmlFor="images" className="drop-container">
+        <div className="inner-border">
+          <img
+            src={document_icon}
+            alt="document_icon"
+            className="document_icon"
+          />
+          <span className="drop-title">
+            Drag & Drop Here or <strong>Browse</strong>
+          </span>
+        </div>
         <input type="file" id="fileUpload" hidden />
         <button className="upload_button" onClick={handleImageUpload}>
           Upload Manifest
@@ -23,4 +27,5 @@ const UploadField = () => {
     </div>
   );
 };
+
 export default UploadField;
