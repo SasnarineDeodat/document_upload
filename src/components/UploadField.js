ae54d77f570984729824f87c5ccf0d1d172316ad
Robbie213 <sasnarinedeodat407@gmail.com>
2024-01-30 10:23:46 -0500

    Implemented the File name showing logic

--- src/components/UploadField.js
+++ src/components/UploadField.js
@@ -1,10 +1,24 @@
 import document_icon from "../assets/document_icon.svg";
 import ProgressBar from "./ProgressBar";
 import "./UploadField.css";
+import { useState } from "react";
 
 const UploadField = () => {
+  const [fileName, setFileName] = useState();
+  const [fileSize, setFileSize] = useState();
+
   const handleImageUpload = () => {
     document.getElementById("fileUpload").click();
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setFileName(file.name);
+    let fileSizeInMB = (file.size / (1024 * 1024)).toFixed(2);
+    if (fileSizeInMB == 0.0) {
+      fileSizeInMB = 0.01;
+    }
+    setFileSize(fileSizeInMB);
   };
 
   return (
@@ -20,17 +34,19 @@ const UploadField = () => {
             Drag & Drop Here or <strong>Browse</strong>
           </span>
         </div>
-        <input type="file" id="fileUpload" hidden />
+        <input type="file" id="fileUpload" hidden onChange={handleFileChange} />
         <button className="upload_button" onClick={handleImageUpload}>
           Upload Manifest
         </button>
       </label>
       <hr className="underline" />
-      <ProgressBar
-        completed={10}
-        text="WN-DAL-0726-NH20166 csv"
-        numbers="5.7MB"
-      />
+      {fileName && (
+        <ProgressBar
+          completed={10}
+          text={fileName}
+          numbers={`${fileSize} MB`}
+        />
+      )}
     </div>
   );
 };
