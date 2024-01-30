9fb8bdc5cf4f60cb0565c4ec2c9576a7182d6efe
Robbie213 <sasnarinedeodat407@gmail.com>
2024-01-29 23:02:47 -0500

    I still have to make the text align straight on small screens

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -10,22 +10,29 @@ const Switch = ({ isOn }) => {
 
   return (
     <>
-      <input
-        className="react-switch-checkbox"
-        id={`react-switch-new`}
-        type="checkbox"
-        checked={checked}
-        onChange={handleToggle}
-      />
-      <label
-        style={{ background: checked && "#134461" }}
-        className="react-switch-label"
-        htmlFor={`react-switch-new`}
-      >
-        <span className={`react-switch-button`} />
-      </label>
-
-      <span className="toggle_on">{checked ? "Toggle ON" : "Toggle OFF"}</span>
+      <div className="switch-container">
+        <input
+          className="react-switch-checkbox"
+          id={`react-switch-new`}
+          type="checkbox"
+          checked={checked}
+          onChange={handleToggle}
+        />
+        <label
+          style={{ background: checked && "#134461" }}
+          className="react-switch-label"
+          htmlFor={`react-switch-new`}
+        >
+          <span className={`react-switch-button`} />
+        </label>
+        <span className="toggle_on">
+          {checked ? "Toggle ON" : "Toggle OFF"} |
+        </span>
+        <div className="image-container">
+          <img height={25} width={25} src="./clock.png" className="clock_img" />
+          <span className="image-text">Select Tolerance Level</span>
+        </div>
+      </div>
     </>
   );
 };
