e80036373284c34c7aafff0f1431a5ae293abbda
Robbie213 <sasnarinedeodat407@gmail.com>
2024-01-29 22:31:45 -0500

    Added the logic to the button

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Switch.css";
 
 const Switch = ({ isOn }) => {
-  const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useState(isOn);
 
   const handleToggle = () => {
     setChecked(!checked);
@@ -18,12 +18,14 @@ const Switch = ({ isOn }) => {
         onChange={handleToggle}
       />
       <label
-        style={{ background: isOn && "#134461" }}
+        style={{ background: checked && "#134461" }}
         className="react-switch-label"
         htmlFor={`react-switch-new`}
       >
         <span className={`react-switch-button`} />
       </label>
+
+      <span className="toggle_on">{checked ? "Toggle ON" : "Toggle OFF"}</span>
     </>
   );
 };
