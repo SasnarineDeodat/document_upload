988a56e7691c041c02b09cf5b899215850166030
Robbie213 <sasnarinedeodat407@gmail.com>
2024-01-30 12:06:08 -0500

    Deleted the useState import

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import "./ProgressBar.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFile } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import "./ProgressBar.css";
 import Switch from "./Switch";
 
 const ProgressBar = ({ completed, text, numbers }) => {
